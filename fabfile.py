82a69a2d702808646b72344a4eac4f0b5b3186d3
Atul Varma <varmaa@gmail.com>
2011-11-11 21:15:20 +0100

    added fabfile

---
+++ fabfile.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+
+try:
+    import fabfile_local
+except ImportError:
+    pass
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+path = lambda *x: os.path.join(ROOT, *x)
+
+sys.path.append(path('vendor'))
+
+@task
+def deploy():
+    run('mkdir -p %s' % env.remote_dir)
+    rsync_project(remote_dir=env.remote_dir,
+                  local_dir='static-files/')
+    print "files placed in %s" % env.serve_dir
+
+@task
+def freeze():
+    import imagepal
+
+    distdir = path('dist')
+    local('mkdir -p %s' % distdir)
+
+    staticdir = path('static-files')
+    local('cp -R %s/ %s/' % (staticdir, distdir))
+
+    filename = 'sample-etherpad-content.html'
+    source = open(os.path.join(staticdir, filename)).read()
+    imagepal.rebase(source, distdir, filename)
